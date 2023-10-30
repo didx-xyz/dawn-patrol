409b58f55615c90f3c4889c9d6caf0ef8496a497
ff137 <ff137@proton.me>
2023-10-30 11:56:39 +0200

    :art:

--- src/xyz/didx/DawnPatrol.scala
+++ src/xyz/didx/DawnPatrol.scala
@@ -1,7 +1,7 @@
 package xyz.didx
 import cats.effect.*
-object DawnPatrol extends IOApp{   
-    def run(args: List[String]): IO[ExitCode] =
-        IO(println("Hello, world!")).as(ExitCode.Success)
-  
+object DawnPatrol extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    IO(println("Hello, world!")).as(ExitCode.Success)
+
 }
