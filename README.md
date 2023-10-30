2fe1701eb311b474d4446723f8697fa4cb97b700
iandebeer <ian@mn8.dev>
2023-10-30 15:29:10 +0200

    release 0.0.1

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 DawnPatrol provides a chatbot interface to messaging services sucg as Signal, Slack, WhatsApp and Telegram.
 It utilizes LLM's such as ChatGPT to provide a concersational capability to support a large variety of client inputs
+Special messages:
+
+- Admin messages: @admin|add <name> <mobilenumber>- allows administrators to add new users to ecosystem
+This will be replaced by uploading .vcf file
+
+- Location data: Upload a Google Maps format pin.
--- project.scala
+++ project.scala
@@ -2,16 +2,19 @@
 //> using toolkit typelevel:latest
 //> using packaging.output "dawn-patrol"
 //> using nativeMode "release-fast"
+//> using resourceDir ./src/resources
+
 //> using dep io.circe::circe-core:0.14.6
 //> using dep io.circe::circe-parser:0.14.6
 //> using dep io.circe::circe-generic:0.14.6
 //> using dep com.github.pureconfig::pureconfig-core:0.17.4
 //> using dep com.github.pureconfig::pureconfig-cats-effect:0.17.4
-//> using dep dev.profunktor::redis4cats-effects:1.4.3
-//> using dep dev.profunktor::redis4cats-log4cats:1.4.3
+//> using dep dev.profunktor::redis4cats-effects:1.5.2
+//> using dep dev.profunktor::redis4cats-log4cats:1.5.2
 
 //> using dep com.softwaremill.sttp.client3::core:3.9.0
 //> using dep com.softwaremill.sttp.client3::circe:3.9.0
 //> using dep com.softwaremill.sttp.client3::async-http-client-backend-cats:3.9.0
 //> using dep de.brendamour:jpasskit:0.3.3
 
+//> using dep org.slf4j:slf4j-nop:2.0.9 
