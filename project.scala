ab02fa8982f1dbfb98ff438ab8860ebda5407c06
ff137 <ff137@proton.me>
2023-10-30 11:50:31 +0200

    :art:

--- project.scala
+++ project.scala
@@ -1,13 +1,12 @@
 //> using scala 3.3.1
 //> using toolkit typelevel:latest
-//> using packaging.output "dawnctl"
+//> using packaging.output "dawn-patrol"
 //> using nativeMode "release-fast"
 //> using dep io.circe::circe-core:0.14.6
 //> using dep io.circe::circe-parser:0.14.6
 //> using dep io.circe::circe-generic:0.14.6
 //> using dep com.github.pureconfig::pureconfig-core:0.17.4
 //> using dep com.github.pureconfig::pureconfig-cats-effect:0.17.4
-
 
 //> using dep com.softwaremill.sttp.client3::core:3.9.0
 //> using dep com.softwaremill.sttp.client3::circe:3.9.0
