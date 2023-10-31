98432c8c779979423f8e7cb0288822f99e58510f
ff137 <ff137@proton.me>
2023-10-31 14:03:47 +0200

    :heavy_plus_sign: add dependencies for AiHandler

--- project.scala
+++ project.scala
@@ -14,9 +14,18 @@
 //> using dep dev.profunktor::redis4cats-effects:1.5.2
 //> using dep dev.profunktor::redis4cats-log4cats:1.5.2
 
+//> using dep com.softwaremill.sttp.client4::core:4.0.0-M6
 //> using dep com.softwaremill.sttp.client3::core:3.9.0
 //> using dep com.softwaremill.sttp.client3::circe:3.9.0
 //> using dep com.softwaremill.sttp.client3::async-http-client-backend-cats:3.9.0
 //> using dep de.brendamour:jpasskit:0.3.3
+
+//> using dep com.outr::scribe:3.12.2
+//> using dep com.lihaoyi::upickle:3.1.3
+//> using dep dev.langchain4j:langchain4j:0.22.0
+//> using dep com.xebia::xef-scala:0.0.3
+//> using dep com.xebia:xef-pdf:0.0.3 //runtime
+//> using dep com.xebia:xef-reasoning-jvm:0.0.3
+//> using dep com.xebia:xef-openai:0.0.3 //runtime.pomonly
 
 //> using dep org.slf4j:slf4j-nop:2.0.9
