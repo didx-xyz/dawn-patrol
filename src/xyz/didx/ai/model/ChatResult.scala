f312d059150ae7780e3ff6473bb5f8ccfe4214ea
ff137 <ff137@proton.me>
2023-11-02 11:04:54 +0200

    :art: add "confirmed" as part of OnboardingResult

--- src/xyz/didx/ai/model/ChatResult.scala
+++ src/xyz/didx/ai/model/ChatResult.scala
@@ -5,8 +5,9 @@ import io.circe.Decoder
 
 case class OnboardingResult(
   @Description("The next message that you want to send to the user") nextMessageToUser: String,
-  @Description("The full name as obtained from the user") fullName: Option[String] = None,
-  @Description("The email address as obtained from the user") email: Option[String] = None,
-  @Description("The cellphone number as obtained from the user") cellphone: Option[String] = None
+  @Description("The full name of the user") fullName: Option[String] = None,
+  @Description("The email address of the user") email: Option[String] = None,
+  @Description("The cellphone number of the user") cellphone: Option[String] = None,
+  @Description("User has confirmed captured fields are correct") confirmed: Boolean = false
 ) derives SerialDescriptor,
       Decoder
