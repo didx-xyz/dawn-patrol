34f3ef113e3ed462b5d65e63c380b05fa8cf830b
ff137 <ff137@proton.me>
2023-11-02 11:05:45 +0200

    :art: use polling interval from config

--- src/xyz/didx/DawnPatrol.scala
+++ src/xyz/didx/DawnPatrol.scala
@@ -12,19 +12,25 @@ import scala.concurrent.duration.*
 import xyz.didx.ConversationPollingHandler
 import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
 import sttp.client3.SttpBackend
+import xyz.didx.config.ConfigReaders.getConf
 import xyz.didx.ai.handler.Opportunities
+
 object DawnPatrol extends IOApp.Simple:
   // override protected def blockedThreadDetectionEnabled = true
-  given logger: Logger[IO]            = Slf4jLogger.getLogger[IO]
-  val pollingInterval: FiniteDuration = 10.seconds
-  val pollingHandler                  = new ConversationPollingHandler(using logger)
-  val run                             = Dispatcher[IO].use { dispatcher =>
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+
+  val appConf = getConf(using logger)
+
+  val pollingInterval: FiniteDuration            = appConf.pollingInterval.seconds
+  val pollingHandler: ConversationPollingHandler = new ConversationPollingHandler(using logger)
+
+  val run = Dispatcher[IO].use { dispatcher =>
     val pollingStream = for {
-      backend: SttpBackend[cats.effect.IO, Any] <- Stream.resource(
-                                                     AsyncHttpClientCatsBackend.resource[IO]()
-                                                   )
-      _                                         <- Stream.fixedRate[IO](10.seconds) // Poll every 10 seconds
-      data                                      <- Stream.eval(pollingHandler.converse(backend))
+      backend: SttpBackend[IO, Any] <- Stream.resource(
+                                         AsyncHttpClientCatsBackend.resource[IO]()
+                                       )
+      _                             <- Stream.fixedRate[IO](pollingInterval)
+      data                          <- Stream.eval(pollingHandler.converse(backend))
     } yield data
 
     val pollingWithLogging = pollingStream.compile.drain
