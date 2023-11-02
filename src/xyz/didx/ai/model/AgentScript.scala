9d8a965641b8bb17b5f599c159880174c6c75a01
ff137 <ff137@proton.me>
2023-11-02 11:04:30 +0200

    :art: refactor script and add telephone to initial context if present

--- src/xyz/didx/ai/model/AgentScript.scala
+++ src/xyz/didx/ai/model/AgentScript.scala
@@ -4,18 +4,29 @@ import com.xebia.functional.xef.prompt.JvmPromptBuilder
 import com.xebia.functional.xef.prompt.PromptBuilder
 
 object AgentScript {
-  def createOnboardingBuilder(): PromptBuilder = // Describe AI agent's assignment
-    new JvmPromptBuilder()
-      .addSystemMessage(
-        "You are an onboarding assistant. " +
-          "If you receive a first message from a user, your job is ask for " +
-          "the following info: " +
-          "Name; Email; Cellphone. " +
-          "Be friendly." +
-          "If they only give one attribute at a time, that's fine, just remind " +
-          "them until you have all 3 fields." +
-          "If a user says no, they don't want to give their email or cellphone, then that is fine, but we at least need a name"
-      )
+  def createOnboardingBuilder(telNo: Option[String] = None): PromptBuilder = {
+    val baseMessage =
+      "You are an onboarding assistant, in charge of obtaining the following information from the user: " +
+        "Name; Email; Cellphone. " +
+        "When receiving your first message from a user, begin asking for the info you still need. " +
+        "They can give one attribute at a time, or all at once. " +
+        "If a user has already given their name, email or cellphone in the chat, you shouldn't ask them again." +
+        "Once you have the Name, Email, and Cellphone, then show the result to the user and ask them to confirm. " +
+        "Be friendly."
+      // "A user is allowed to reject giving their email or cellphone. If they refuse, fill the result with `<rejected>`. " +
+      // "It is only the Name that is required."
+
+    val telNoMessage = telNo match {
+      case Some(number) =>
+        s"We already know the cellphone number of the user: it is $number. You don't need to ask for the cellphone number. " +
+          "This means, only ask them for their Name and Email. I repeat. You already know the cell number. Only ask for their Name and Email."
+      case None         => ""
+    }
+
+    val fullMessage = s"$baseMessage $telNoMessage"
+
+    new JvmPromptBuilder().addSystemMessage(fullMessage)
+  }
 
   def createOpportunityBuilder(): PromptBuilder =
     new JvmPromptBuilder()
