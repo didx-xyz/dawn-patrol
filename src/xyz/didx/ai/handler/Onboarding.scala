6dfc76e9ce077b24197a830200f0f018bcc28ad3
Mourits de Beer <31511766+ff137@users.noreply.github.com>
2023-11-17 13:20:31 +0200

    Update usage of conversationId (#13)
    * don't use conversationId in confirmation state
    
    * randomise xef conversationId and reset if cleanSlate

--- src/xyz/didx/ai/handler/Onboarding.scala
+++ src/xyz/didx/ai/handler/Onboarding.scala
@@ -7,10 +7,12 @@ import com.xebia.functional.xef.scala.conversation.*
 import com.xebia.functional.xef.store.ConversationId
 import xyz.didx.ai.model.OnboardingResult
 import xyz.didx.ai.model.AgentScript
+import scala.util.Random
 
 object OnboardingHandler {
   // Define a map from conversationId to JvmPromptBuilder
   private val builders: mutable.Map[String, PromptBuilder] = mutable.Map()
+  private val conversationIds: mutable.Map[String, String] = mutable.Map()
 
   def getResponse(
     input: String,
@@ -23,26 +25,30 @@ object OnboardingHandler {
     )
 
     val defaultPromptBuilder   = AgentScript.createYomaOnboardingBuilder(telNo)
+    val randomString: String   = Random.alphanumeric.take(10).mkString
     val builder: PromptBuilder = cleanSlate match
       case true  =>
         // We want to restart the onboarding on a clean slate, so update builders and use default
+        conversationIds.update(conversationId, randomString) // reset conversationId
         builders.update(conversationId, defaultPromptBuilder)
         defaultPromptBuilder
       case false =>
         // Get the builder for this conversationId, or create a new one if it doesn't exist
+        conversationIds.getOrElseUpdate(conversationId, randomString) // update conversationId if not exists
         builders.getOrElseUpdate(conversationId, defaultPromptBuilder)
 
     // Add the user message to the builder
     builder.addUserMessage(input)
 
     // Create a new conversation with the specific conversationId
+    val xefConversationId = Some(ConversationId(conversationIds.getOrElse(conversationId, "default")))
     conversation(
       {
         val result = prompt[OnboardingResult](builder.build())
         scribe.info(f"We have the following onboarding result: $result")
         result
       },
-      conversationId = Some(ConversationId(conversationId))
+      conversationId = xefConversationId
     )
   }
 }
